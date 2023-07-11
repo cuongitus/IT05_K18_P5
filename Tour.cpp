066f40db3ef9f08c1cd6ab480b0c932610b10ea2
PhuongHM <40162878+hmp18091996@users.noreply.github.com>
2023-07-11 15:43:20 +0700

    Update code

--- Tour.cpp
+++ Tour.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstring>
-
+using namespace std;
 
 // Khai bao danh sach lien ket don chua danh sach Tour
 struct Tour{
@@ -20,31 +20,7 @@ struct ListTour{
     NodeTour *Tail;
 };
 
-void nhapTour(Tour &tour){
-
-    std::cout << "Ma Tour :";
-    fflush(stdin); gets(tour.maTour);
-
-    std::cout << "Ten Tour :";
-    fflush(stdin); gets(tour.tenTour);  
-
-    std::cout << "So luong hanh khach :";
-    scanf("%d", &tour.giaTour);  
-
-    std::cout << "Gia Tour :";
-    scanf("%d", &tour.soLuongHanhKhach);  
-
-}
-
-void inTour(Tour tour){
-
-    std::cout << "Ma Tour: " <<  tour.maTour <<  std::endl;
-    std::cout << "Ten Tour: " <<  tour.tenTour << std::endl;
-    std::cout << "Gia Tour: " <<  tour.giaTour << std::endl;
-    std::cout <<  "So luong hanh khach: " << tour.soLuongHanhKhach << std::endl;
-
-}
-
+// Khoi tao List
 void init (ListTour &Q){
     
         Q.Head = NULL;
@@ -57,7 +33,7 @@ NodeTour* getNodeTour(Tour tour){
     p = new NodeTour();
 
     if(p == NULL){
-        std::cout << "Chua co tour nao duoc khai bao" <<  std::endl;
+        cout << "Chua co tour nao duoc khai bao" <<  endl;
         exit(0);
     }
 
@@ -67,6 +43,33 @@ NodeTour* getNodeTour(Tour tour){
     return p;
 }
 
+// Xu ly nhap/in Tour
+void nhapTour(Tour &tour){
+
+    cout << "Ma Tour :";
+    fflush(stdin); gets(tour.maTour);
+
+    cout << "Ten Tour :";
+    fflush(stdin); gets(tour.tenTour);  
+
+    cout << "So luong hanh khach :";
+    scanf("%d", &tour.giaTour);  
+
+    cout << "Gia Tour :";
+    scanf("%d", &tour.soLuongHanhKhach);  
+
+}
+
+void inTour(Tour tour){
+
+    cout << "Ma Tour: " <<  tour.maTour <<  endl;
+    cout << "Ten Tour: " <<  tour.tenTour << endl;
+    cout << "Gia Tour: " <<  tour.giaTour << endl;
+    cout <<  "So luong hanh khach: " << tour.soLuongHanhKhach << endl;
+
+}
+
+// Xu ly chen Tour
 void ChenDauTour(ListTour &Q, NodeTour *p){
 
     if(Q.Head == NULL){
@@ -89,6 +92,7 @@ void ChenCuoiTour(ListTour &Q, NodeTour *p){
         Q.Tail = p;
     }
 }
+
 
 void xoaDauTour(ListTour &Q){
 
@@ -136,6 +140,18 @@ int xoaNodeTour_ma(ListTour &Q, char s[]){
     }
 
     return 1;
+}
+
+// Dem Tour
+int demTour(ListTour Q){
+
+    NodeTour *p;
+    int count = 0;
+
+    for (p  = Q.Head; p!= NULL; p = p -> next){
+        count ++;
+    }
+    return count;
 }
 
 // Tim kiem Tour
@@ -202,7 +218,7 @@ void suaNodeTour_ma(ListTour &Q, char maTour[], char tenTourMoi[], int giaTourMo
         p -> info.giaTour = giaTourMoi;
         p -> info.soLuongHanhKhach = soLuongMoi;
     }else{
-        std::cout << "Khong co tour ban chon:" <<  std::endl;
+        cout << "Khong co tour ban chon:" <<  endl;
     }   
 }
 
@@ -213,13 +229,19 @@ void nhapDSTour(ListTour &Q){
     NodeTour *p;
     Tour tour;
 
-    std::cout << "Nhap so tour: ";
+    cout << "Nhap so tour: ";
     scanf("%d", &n);
 
     for(i = 0; i < n; i ++){
+
         nhapTour(tour);
         p = getNodeTour(tour);
-        ChenCuoiTour(Q,p);
+
+        if(TimNodeTour_ma(Q, p->info.maTour) == NULL){
+            ChenCuoiTour(Q,p);
+        }else{
+            cout << "Da ton tai Ma Tour nay trong danh sach" << endl;
+        }        
     }
 }
 
@@ -228,14 +250,236 @@ void inDsTour(ListTour Q){
     NodeTour *p;
     int count  = 1;
 
-    std::cout << "---------- Danh sách Tour ----------" << std::endl;
+    cout << "---------- Danh sách Tour ----------" << endl;
 
     for(p  = Q.Head; p!= NULL ; p = p -> next){
 
-        std::cout << "---------- Tour:" << count << "----------" << std::endl;
+        cout << "---------- Tour:" << count << "----------" << endl;
         inTour(p ->info);
 
         count ++;
     }
 }
 
+// Xu ly sap xep
+int sapXepTourGiaTang(ListTour &Q){
+    
+    NodeTour *p;
+    NodeTour *q;
+
+    Tour tour;
+
+    if(Q.Head == NULL){
+        return 0;
+    }
+
+    for(p = Q.Head ; p != NULL ; p = p -> next){
+        for(q = p->next ; q != NULL ; q = q -> next){
+            if(p->info.giaTour > q -> info.giaTour){
+                tour = p -> info;
+                p -> info = q -> info;
+                q -> info = tour;
+            }
+        }
+    }
+    
+    return 1;
+
+}
+
+int sapXepTourGiaGiam(ListTour &Q){
+    
+    NodeTour *p;
+    NodeTour *q;
+
+    Tour tour;
+
+    if(Q.Head == NULL){
+        return 0;
+    }
+
+    for(p = Q.Head ; p != NULL ; p = p -> next){
+        for(q = p->next ; q != NULL ; q = q -> next){
+            if(p->info.giaTour < q -> info.giaTour){
+                tour = p -> info;
+                p -> info = q -> info;
+                q -> info = tour;
+            }
+        }
+    }
+    
+    return 1;
+
+}
+
+
+int sapXepTourSoluongTang(ListTour &Q){
+    
+    NodeTour *p;
+    NodeTour *q;
+
+    Tour tour;
+
+    if(Q.Head == NULL){
+        return 0;
+    }
+
+    for(p = Q.Head ; p != NULL ; p = p -> next){
+        for(q = p->next ; q != NULL ; q = q -> next){
+            if(p->info.soLuongHanhKhach > q -> info.soLuongHanhKhach){
+                tour = p -> info;
+                p -> info = q -> info;
+                q -> info = tour;
+            }
+        }
+    }
+    
+    return 1;
+
+}
+
+int sapXepTourSoluongGiam(ListTour &Q){
+    
+    NodeTour *p;
+    NodeTour *q;
+
+    Tour tour;
+
+    if(Q.Head == NULL){
+        return 0;
+    }
+
+    for(p = Q.Head ; p != NULL ; p = p -> next){
+        for(q = p->next ; q != NULL ; q = q -> next){
+            if(p->info.soLuongHanhKhach < q -> info.soLuongHanhKhach){
+                tour = p -> info;
+                p -> info = q -> info;
+                q -> info = tour;
+            }
+        }
+    }
+    
+    return 1;
+
+}
+
+// Cac yeu cau tim phan tu lon nhat, nho nhat
+
+void tour_soluong_max(ListTour Q){
+
+    NodeTour *p;
+    int max = p->info.soLuongHanhKhach;
+
+    for(p = Q.Head ; p != NULL ; p = p ->next){
+        if(p->info.soLuongHanhKhach > max){
+            max = p -> info.soLuongHanhKhach;
+        }
+    }
+
+    for (p = Q.Head ; p != NULL ; p = p ->next){
+        if(p->info.soLuongHanhKhach == max){
+            inTour(p ->info);
+        }
+    }
+}
+
+void tour_soluong_min(ListTour Q){
+    
+    NodeTour *p;
+    int min = p->info.soLuongHanhKhach;
+
+    for(p = Q.Head ; p != NULL ; p = p ->next){
+        if(p->info.soLuongHanhKhach < min){
+            min = p -> info.soLuongHanhKhach;
+        }
+    }
+
+    for (p = Q.Head ; p != NULL ; p = p ->next){
+        if(p->info.soLuongHanhKhach == min){
+            inTour(p ->info);
+        }
+    }
+}
+
+void tour_gia_max(ListTour Q){
+    
+    NodeTour *p;
+    int max = p->info.giaTour;
+
+    for(p = Q.Head ; p != NULL ; p = p ->next){
+        if(p->info.giaTour > max){
+            max = p -> info.giaTour;
+        }
+    }
+
+    for (p = Q.Head ; p != NULL ; p = p ->next){
+        if(p->info.giaTour == max){
+            inTour(p ->info);
+        }
+    }
+}
+
+void tour_gia_min(ListTour Q){
+    
+    NodeTour *p;
+    int min = p->info.giaTour;
+
+    for(p = Q.Head ; p != NULL ; p = p ->next){
+        if(p->info.giaTour < min){
+            min = p -> info.giaTour;
+        }
+    }
+
+    for (p = Q.Head ; p != NULL ; p = p ->next){
+        if(p->info.giaTour == min){
+            inTour(p ->info);
+        }
+    }
+}
+
+// Xu ly luu/doc file
+
+void luuFileTour(FILE *f, ListTour Q, const char *tenFile){
+
+    f = fopen(tenFile, "wb");
+    int n = 0;
+    NodeTour *p;
+
+    for(p = Q.Head ; p != NULL ; p = p -> next){
+        n ++;
+    }
+
+    fwrite(&n, sizeof(int), 1, f);
+
+    for(p = Q.Head ; p != NULL ; p = p -> next){
+        fwrite(&p -> info , sizeof(Tour), 1, f);
+    }
+
+    fclose(f);
+}
+
+void docFileTour(FILE *f, ListTour Q, const char *tenFile){
+
+    Tour x;
+    int i,n;
+
+    NodeTour *p;
+
+    f = fopen(tenFile,"rb");
+
+    if (f == NULL) {
+        cout << "Khong the mo file " << tenFile << " de doc." << endl;
+        return;
+    }
+
+    fread(&n, sizeof(int), 1, f);
+
+    for(i = 0; i < n ; i++){
+        fread(&x, sizeof(Tour), 1, f);
+        p = getNodeTour(x);
+        ChenDauTour(Q,p);  
+    }
+    fclose(f);
+
+    inDsTour(Q);
+}
--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@ int main(){
     ListTour Q;
     NodeTour *p;
 
+    FILE *f;
+    char *tenFile = "danhSachTour.bin"; 
+
+    init(Q);
+
     int key;
     int kq;
 
@@ -17,6 +22,8 @@ int main(){
     char tenTourMoi[20];
     int giaTourMoi;
     int soLuongHanhKhachMoi;
+    
+
     
 
     while (true){
@@ -28,14 +35,25 @@ int main(){
         cout << "==  4. Xem danh sach Tour du lich                                   ==" << endl;
         cout << "==  5. Tim kiem Tour - Ma                                           ==" << endl;
         cout << "==  6. Tim kiem Tour - Ten                                          ==" << endl;
+        cout << "==  7. Sap xep Tour - Gia tour tang dang                            ==" << endl;
+        cout << "==  8. Sap xep Tour - Gia tour giam dang                            ==" << endl;
+        cout << "==  9. Sap xep Tour - So luong hanh khach tang dang                 ==" << endl;
+        cout << "==  10. Sap xep Tour - So luong hanh khach giam dang                ==" << endl;
+        cout << "==  11. Tour có gia cao nhat                                        ==" << endl;
+        cout << "==  12. Tour có gia thap nhat                                       ==" << endl;
+        cout << "==  12. Tour có so luong khach cao nhat                             ==" << endl;
+        cout << "==  14. Tour có so luong khach thap nhat                            ==" << endl;
+        cout << "==  15. Tong so luong Tour hien co                                  ==" << endl;
+        cout << "==  16.                                   ==" << endl;
+        cout << "==  21. Luu Ds tour ra file csv                                     ==" << endl;
         cout << "======================================================================" << endl;
         cout << "Nhap tuy chon: ";
         cin >> key;
 
         switch (key){
+
         case 1:
             cout << "1. Them tour du lich." << endl;
-            init(Q);
             nhapDSTour(Q);
             break;
 
@@ -53,7 +71,6 @@ int main(){
             cout << "Nhap so luong Tour moi: ";
             cin >> soLuongHanhKhachMoi;
             
-
             suaNodeTour_ma(Q, maTour, tenTourMoi, giaTourMoi, soLuongHanhKhachMoi);
 
             break;
@@ -95,6 +112,89 @@ int main(){
             }
             break;
 
+        case 7:
+            cout << "7. Sap xep Tour - Gia tour tang dang." << endl;
+
+            kq = sapXepTourGiaTang(Q);
+
+            if(kq == 0){
+                cout << "Danh sach Tour dang trong." << endl;
+            }else{
+                cout << "Sap xep thanh cong." << endl;
+            }
+            break;
+        
+        case 8:
+            cout << "8. Sap xep Tour - Gia tour giam dang." << endl;
+
+            kq = sapXepTourGiaGiam(Q);
+
+            if(kq == 0){
+                cout << "Danh sach Tour dang trong." << endl;
+            }else{
+                cout << "Sap xep thanh cong." << endl;
+            }
+            break;
+
+        case 9:
+            cout << "9. Sap xep Tour - So luong tang dang." << endl;
+
+            kq = sapXepTourSoluongTang(Q);
+
+            if(kq == 0){
+                cout << "Danh sach Tour dang trong." << endl;
+            }else{
+                cout << "Sap xep thanh cong." << endl;
+            }
+            break;
+        
+        case 10:
+            cout << "10. Sap xep Tour - So luong giam dang." << endl;
+
+            kq = sapXepTourSoluongGiam(Q);
+
+            if(kq == 0){
+                cout << "Danh sach Tour dang trong." << endl;
+            }else{
+                cout << "Sap xep thanh cong." << endl;
+            }
+            break;
+        
+        case 11:
+            cout << "11. Danh sach Tour co gia cao nhat" << endl;
+            tour_soluong_max(Q);
+            break;
+
+        case 12:
+            cout << "12. Danh sach Tour co gia thap nhat" << endl;
+            tour_soluong_max(Q);
+            break;
+            
+        case 13:
+            cout << "13. Danh sach Tour co so luong khach cao nhat" << endl;
+            tour_soluong_max(Q);
+            break;
+
+        case 14:
+            cout << "14. Danh sach Tour co so luong khach thap nhat" << endl;
+            tour_soluong_max(Q);
+            break;
+
+        case 15:
+            cout << "15. Tong so luong Tour hien co: " << demTour(Q) << endl;
+            break;
+
+        case 21:
+            cout << "21. Luu Ds tour ra file csv" << endl;
+            luuFileTour(f, Q, tenFile);
+            break; 
+
+        case 22:
+            cout << "22. Doc Ds tour ra file csv" << endl;
+            docFileTour(f, Q, tenFile);
+            break;
+            
+
         default:
             cout << "Khong co chuc nang nay trong menu." << endl;
             cout << "Vui long chon chuc nang trong hop menu" << endl;
@@ -102,5 +202,5 @@ int main(){
         }
     }
 
-    inDsTour(Q);
+    exit(0);
 }
